a349d13a257edd268a37cc067fde193d5c67a4d5
visualfc <visualfc@gmail.com>
2018-01-30 22:02:48 +0800

    update keyboard

--- keyboard/main.go
+++ keyboard/main.go
@@ -2,62 +2,56 @@ package main
 
 import (
 	"fmt"
-	"log"
-	"time"
 
 	"github.com/visualfc/atk/event"
 	"github.com/visualfc/atk/tk"
 )
 
-type Event struct {
-	Widget tk.Widget
-	Time   time.Time
+type Window struct {
+	*tk.Window
+	keyLabel *tk.Label
+	posLabel *tk.Label
 }
 
-type MouseEvent struct {
-	Event
-	Button int
-	PosX   int
-	PosY   int
+func NewWindow() *Window {
+	w := tk.MainWindow()
+	key := tk.NewLabel(w, "keyboard")
+	pos := tk.NewLabel(w, "pos")
+	key.SetFont(tk.NewUserFont("", 24))
+
+	vpk := tk.NewVPackLayout(w)
+	vpk.AddWidget(key, tk.PackAttrFillBoth(), tk.PackAttrExpand(true))
+	vpk.AddWidget(pos, tk.PackAttrAnchor(tk.AnchorSouthEast))
+
+	win := &Window{w, key, pos}
+	win.BindKeyEvent(win.OnKeyEvent)
+	win.BindEvent(event.Motion, win.OnMotion)
+	win.BindEvent(event.ButtonPress, win.OnButtonClick)
+	win.BindEvent("<Double-ButtonPress>", win.OnButtonDbclick)
+	win.SetTitle("Keyboard")
+	return win
 }
 
-type WhellEvent struct {
-	MouseEvent
-	Whell int
+func (w *Window) OnKeyEvent(e *tk.KeyEvent) {
+	w.keyLabel.SetText(fmt.Sprintf("Code:\t0x%x\nSym:\t%v\nText:\t%v\nRune:\t%x\nModifier:\t%v",
+		e.KeyCode, e.KeySym, e.KeyText, e.KeyRune, e.KeyModifier))
 }
 
-type KeyEvent struct {
-	Event
-	Key  int
-	Text string
+func (w *Window) OnMotion(e *tk.Event) {
+	w.posLabel.SetText(fmt.Sprintf("global: %v-%v\tpos: %3d-%3d", e.GlobalPosX, e.GlobalPosY, e.PosX, e.PosY))
+}
+
+func (w *Window) OnButtonClick(e *tk.Event) {
+	w.posLabel.SetText(fmt.Sprintf("mouse click: %v\tpos: %v-%v", e.MouseButton, e.PosX, e.PosY))
+}
+
+func (w *Window) OnButtonDbclick(e *tk.Event) {
+	w.posLabel.SetText(fmt.Sprintf("mouse double click: %v\tpos: %v-%v", e.MouseButton, e.PosX, e.PosY))
 }
 
 func main() {
-	if tk.Init() != nil {
-		log.Fatalln("load tk false")
-	}
 	tk.MainLoop(func() {
-		mw := tk.MainWindow()
-		info := tk.NewLabel(mw, "keyboard")
-		info.SetFont(tk.NewUserFont("", 36))
-		pos := tk.NewLabel(mw, "pos:")
-		vpk := tk.NewVPackLayout(mw)
-		vpk.AddWidget(info, tk.PackAttrFillY(), tk.PackAttrExpand(true), tk.PackAttrAnchor(tk.AnchorWest))
-		vpk.AddWidget(pos, tk.PackAttrAnchor(tk.AnchorSouthEast))
-
-		mw.BindEvent(event.KeyPress, func(e *tk.Event) {
-			info.SetText(fmt.Sprintf("Scan:\t0x%x\nSym:\t%v\nText:\t%v", e.KeyScanCode, e.KeySym, e.KeyText))
-		})
-		mw.BindEvent(event.Motion, func(e *tk.Event) {
-			pos.SetText(fmt.Sprintf("global: %v-%v\tpos: %3d-%3d", e.GlobalPosX, e.GlobalPosY, e.PosX, e.PosY))
-		})
-		mw.BindEvent(event.ButtonPress, func(e *tk.Event) {
-			pos.SetText(fmt.Sprintf("mouse click: %v\tpos: %v-%v", e.MouseButton, e.PosX, e.PosY))
-		})
-		mw.BindEvent("<Double-ButtonPress>", func(e *tk.Event) {
-			pos.SetText(fmt.Sprintf("mouse double click: %v\tpos: %v-%v", e.MouseButton, e.PosX, e.PosY))
-		})
-
+		mw := NewWindow()
 		mw.SetSizeN(400, 300)
 		mw.Center()
 		mw.ShowNormal()
