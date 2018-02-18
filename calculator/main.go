fb7ba7260464f827b1c740146987e6ef75782c3d
visualfc <visualfc@gmail.com>
2018-02-18 14:15:07 +0800

    update calc

--- calculator/main.go
+++ calculator/main.go
@@ -2,7 +2,8 @@ package main
 
 import (
 	"fmt"
-	"image"
+	"math"
+	"strconv"
 	"strings"
 
 	"github.com/visualfc/atk/tk"
@@ -10,12 +11,12 @@ import (
 
 func main() {
 	tk.MainLoop(func() {
-		tk.SetMainTheme(nil)
+		//tk.SetMainTheme(nil)
 		w := NewWindow()
 		w.SetTitle("Go 计算器")
 		w.Center()
 		w.ShowNormal()
-		w.SetResizable(false, false)
+		//	w.SetResizable(false, false)
 	})
 }
 
@@ -29,109 +30,148 @@ const (
 
 type MainWindow struct {
 	*tk.Window
-	edit   *tk.Entry
-	font   *tk.UserFont
-	number string
+	edit       *tk.Entry
+	waitNext   bool
+	number     string //数字记录
+	operator   string //运算符 + - × ÷
+	prevNumber float64
+}
+
+func (w *MainWindow) inputNegative() {
+	if w.waitNext {
+		w.waitNext = false
+		w.number = "0"
+	}
+	if strings.HasPrefix(w.number, "-") {
+		w.number = w.number[1:]
+	} else {
+		w.number = "-" + w.number
+	}
+	w.updateText()
 }
 
 func (w *MainWindow) inputNumber(s string) {
+	if w.waitNext {
+		w.waitNext = false
+		w.number = "0"
+	}
 	if s == "." && strings.Contains(w.number, ".") {
 		return
 	}
+	if w.number == "0" || w.number == "-0" {
+		if s == "0" {
+			return
+		} else if s != "." {
+			if w.number == "0" {
+				w.number = ""
+			} else {
+				w.number = "-"
+			}
+		}
+	}
 	w.number += s
+	w.updateText()
+}
+
+func (w *MainWindow) updateText() {
 	w.edit.SetText(w.number)
 }
 
-func (w *MainWindow) inputCmd() {
-
+func (w *MainWindow) currentNumber() float64 {
+	r, _ := strconv.ParseFloat(w.number, 0)
+	return r
 }
 
-type Button struct {
-	*tk.Button
-}
-
-func (btn *Button) OnCommand(fn func()) {
-	btn.Button.OnCommand(fn)
-}
-
-func (w *MainWindow) NewButton(text string) *Button {
-	return w.NewButtonEx(text, 30, 30)
-}
-
-func (w *MainWindow) NewButtonEx(text string, width int, height int) *Button {
-	blank := image.NewNRGBA(image.Rect(0, 0, width, height))
-	img := tk.NewImage()
-	img.SetImage(blank)
-	btn := &Button{}
-	btn.Button = tk.NewButton(w, text)
-	btn.SetWidth(0)
-	if !tk.HasTheme() {
-		btn.SetWidth(width)
+func (w *MainWindow) calculate(operator string) {
+	if w.operator == "" {
+		w.prevNumber = w.currentNumber()
+	} else if !w.waitNext {
+		switch w.operator {
+		case "+":
+			w.prevNumber += w.currentNumber()
+		case "-":
+			w.prevNumber -= w.currentNumber()
+		case "×", "*":
+			w.prevNumber *= w.currentNumber()
+		case "÷", "/":
+			w.prevNumber /= w.currentNumber()
+		}
+		w.number = fmt.Sprintf("%v", w.prevNumber)
 	}
-	btn.SetImage(img)
-	btn.SetCompound(tk.CompoundCenter)
-	btn.SetAttributes(tk.WidgetAttrFont(w.font), tk.WidgetAttrBorderStyle(tk.BorderStyleGroove))
-	return btn
+	w.operator = operator
+	w.waitNext = true
+	w.updateText()
 }
+
+func (w *MainWindow) inputSymbol(s string) {
+	fmt.Println(s)
+	switch s {
+	case "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", ".":
+		w.inputNumber(s)
+	case "π":
+		w.number = fmt.Sprintf("%v", math.Pi)
+		w.updateText()
+	case "+/-":
+		w.inputNegative()
+	case "C", "c": //清除
+		w.number = "0"
+		w.updateText()
+	case "+", "-", "×", "÷", "*", "/":
+		w.calculate(s)
+	case "=":
+		w.calculate(w.operator)
+		w.operator = ""
+	}
+}
+
+var (
+	symbols = []string{
+		"C", "+/-", "%", "÷",
+		"7", "8", "9", "×",
+		"4", "5", "6", "-",
+		"1", "2", "3", "+",
+		"0", "π", ".", "="}
+)
 
 func NewWindow() *MainWindow {
 	mw := &MainWindow{}
 	mw.Window = tk.MainWindow()
-	mw.font = tk.LoadSysFont(tk.SysTextFont).Clone()
-	mw.font.SetSize(20).SetBold(true)
+
+	font := tk.LoadSysFont(tk.SysTextFont).Clone()
+	font.SetSize(20).SetBold(true)
 
 	mw.edit = tk.NewEntry(mw)
 	mw.edit.SetAlignment(tk.AlignmentRight)
+	mw.edit.SetFont(font)
 	mw.edit.SetState(tk.StateReadOnly)
-	mw.edit.SetFont(mw.font)
+	mw.edit.SetText("0")
+	mw.number = "0"
+
 	mw.BindEvent("<Key>", func(e *tk.Event) {
-		if e.KeyRune >= '0' && e.KeyRune <= '9' || e.KeyRune == '.' {
-			mw.inputNumber(e.KeyText)
+		if e.KeySym == "Return" {
+			mw.inputSymbol("=")
+		} else {
+			mw.inputSymbol(e.KeyText)
 		}
 	})
 
-	var number [10]*Button
-	for i := 0; i < 10; i++ {
-		text := fmt.Sprintf("%v", i)
-		number[i] = mw.NewButton(text)
-		number[i].OnCommand(func() {
-			mw.inputNumber(text)
-		})
-	}
-
-	decimal := mw.NewButton(".")
-	decimal.OnCommand(func() {
-		mw.inputNumber(".")
-	})
-
-	clear := mw.NewButton("C")
-
-	sig := mw.NewButton("+/-")
-
-	precent := mw.NewButton("%")
-
-	// + - * /
-	plus := mw.NewButton("+")
-	minus := mw.NewButton("-")
-	multiple := mw.NewButton("×")
-	division := mw.NewButton("÷")
-	equal := mw.NewButton("=")
-
 	grid := tk.NewGridLayout(mw)
-	grid.AddWidgets(clear, nil, precent, division)
-	grid.AddWidgets(number[7], number[8], number[9], multiple)
-	grid.AddWidgets(number[4], number[5], number[6], minus)
-	grid.AddWidgets(number[1], number[2], number[3], plus)
-	grid.AddWidgets(number[0], sig, decimal, equal)
-
-	hbox := tk.NewHPackLayout(mw)
-	hbox.AddWidget(tk.NewLayoutSpacer(mw, 0, true), tk.PackAttrFillX(), tk.PackAttrExpand(true))
-	hbox.AddWidget(grid, tk.PackAttrExpand(false), tk.PackAttrAnchor(tk.AnchorWest))
+	for n, sym := range symbols {
+		btn := tk.NewButton(mw, sym)
+		btn.OnCommand(func() {
+			mw.inputSymbol(btn.Text())
+		})
+		row := n / 4
+		col := n % 4
+		grid.AddWidgetEx(btn, row, col, 1, 1, tk.StickyAll)
+	}
+	grid.SetColumnAttr(-1, 0, 1, "")
+	grid.SetRowAttr(-1, 0, 1, "")
 
 	vbox := tk.NewVPackLayout(mw)
-	vbox.AddWidget(mw.edit, tk.PackAttrFillX(), tk.PackAttrExpand(false), tk.PackAttrAnchor(tk.AnchorNorth))
-	vbox.AddWidget(hbox, tk.PackAttrFillX(), tk.PackAttrExpand(false), tk.PackAttrAnchor(tk.AnchorNorth))
-	//vbox.AddWidget(tk.NewLayoutSpacer(mw, 0, true), tk.PackAttrFillY(), tk.PackAttrExpand(true))
-	vbox.Repack()
+	vbox.SetBorderWidth(4)
+	vbox.AddWidgetEx(mw.edit, tk.FillX, false, 0)
+	vbox.AddWidgetEx(grid, tk.FillBoth, true, 0)
+
 	return mw
 }
